4c5d3136975f4cf2f95e3b5c2c4c72fe21e09065
Eric Stiens <estiens@users.noreply.github.com>
2024-08-27 21:52:49 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './hugo_stats.json',
+    './assets/css/*.css'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
